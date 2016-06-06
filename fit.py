7e1e9ecc11bfc920202af0905803f85f9fc46a30
AndrewJSchoen <schoen.andrewj@gmail.com>
2016-06-06 13:25:34 -0500

    0.1 Comments and Suggestions
    [1] Added a bunch of comments, and moved some stuff around.

--- fit.py
+++ fit.py
@@ -9,7 +9,7 @@ from dipy.align.reslice import reslice
 from dipy.core.gradients import gradient_table
 from dipy.io import read_bvals_bvecs
 from docopt import docopt
-import dipy.data as dpd
+#import dipy.data as dpd
 reload(dti)
 
 import matplotlib.pyplot as plt
@@ -17,10 +17,10 @@ import matplotlib.pyplot as plt
 Version = "0.1"
 
 doc = """
-Orientation Check, Version {0}.
+Model Fitting, Version {0}.
 
 Usage:
-    orientationcheck.py [options] --image=<FILE> --bval=<FILE> --bvec=<FILE> --outprefix=<FILE> [--slice <slice>]
+    orientationcheck.py [options] --image=<FILE> --bval=<FILE> --bvec=<FILE> --outprefix=<FILE> [(--axis s | --axis c | --axis a) --slice <slice>]
 
 Options:
     -h --help          Show this screen.
@@ -53,7 +53,19 @@ def exists(path):
         print("Error: Input file '{0}' not found!".format(path))
         return(0)
 
+def fit(image, gtab, fit_type="WLS"):
+    print("Generating the tensor model.")
+    dti_wls = dti.TensorModel(gtab, fit_method=fit_type)
+
+    ## the fit method will take in the data, regardless of shape.
+    print("Fitting {0} data.".format(view))
+    fit_wls = dti_wls.fit(image)
+
+    return (fit_wls)
+
+
 def run(rawargs):
+    #This gets us our arguments. May need to be tweaked for the new ones we are adding.
     arguments = docopt(doc, argv=rawargs, version='Orientation Check v{0}'.format(Version))
     inputs = [{"Value":"image file", "Flag": "--image"}, {"Value":"bvec file", "Flag": "--bvec"}, {"Value":"bvec file", "Flag": "--bvec"}]
     for inputinfo in inputs:
@@ -61,76 +73,86 @@ def run(rawargs):
             print("The {0} specified does not exist!".format(inputinfo["Value"]))
             sys.exit(1)
 
-    try:
-        image = nib.load(arguments["--image"])
-        bvals, bvecs = read_bvals_bvecs(arguments['--bval'], arguments['--bvec'])
-        #print(bvals)
-        #print(bvecs)
-        print("Generating gradient table.")
-        gtab = gradient_table(bvals, bvecs)
 
-        #Define the tensor model
-        print("Generating the tensor model.")
-        dti_wls = dti.TensorModel(gtab, fit_method="NLLS")
+    #Load image. Always do this.
+    image = nib.load(arguments["--image"])
 
-        image_data = image.get_data()
+    #Create gradient table. Always do this.
+    print("Generating gradient table.")
+    gtab = gradient_table(read_bvals_bvecs(arguments['--bval'], arguments['--bvec']))
 
-
-        print("Masking the brain.")
-        image_masked, mask = median_otsu(image_data, 3, 1, autocrop=True, dilate=2)
-
-        print("Resampling the brain to a standard resolution.")
-        image, affine1 = reslice(image_masked, image.get_affine(), image.get_header().get_zooms()[:3], (3.0,3.0,3.0))
-
-        print("Checking the image dimensions")
-        Xsize, Ysize, Zsize, directions = image.shape
-        print("X: {0}\nY: {1}\nZ: {2}".format(Xsize, Ysize, Zsize))
-
-        #Define Image Scopes
-        print("Defining the image scopes.")
-        imagedict = {"axial": {"dropdim": [0,1], "scope": (slice(0,Xsize), slice(0,Ysize), slice(math.floor(Zsize/2),math.floor(Zsize/2)+1))},
-                     "coronal": {"dropdim": [0,2], "scope": (slice(0,Xsize), slice(math.floor(Ysize/2),math.floor(Ysize/2)+1), slice(0, Zsize))},
-                     "sagittal": {"dropdim": [1,2], "scope": (slice(math.floor(Xsize/2),math.floor(Xsize/2)+1), slice(0,Ysize), slice(0, Zsize))}}
+    #### MOVED ABOVE >>>>>
+    #Define the tensor model
+    # print("Generating the tensor model.")
+    # dti_wls = dti.TensorModel(gtab, fit_method="NLLS")
+    #### MOVED ABOVE <<<<<
 
 
-        #roi_idx = (slice(0,image.shape[0]), slice(0,image.shape[1]), slice(middleslice,middleslice+1))#(slice(0,image.shape[0]), slice(0,image.shape[1]), slice(int(image.shape[2]/2),int(image.shape[2]/2)+1))
-        print("Defining sphere.")
-        sphere = dpd.get_sphere('symmetric724')
+    #Keep this here
+    print("Masking the brain.")
+    image_masked, mask = median_otsu(image_data, 3, 1, autocrop=True, dilate=2)
+    #If the user provides a mask, we could try to mask with it instead
 
-        #Slice the whole dataset by the scope
-        print("Slicing the dataset with the scopes.")
-        for view in ["sagittal", "coronal", "axial"]:
-            imagedict[view]["image"] = image[imagedict[view]["scope"]]
-            print("Fitting {0} data.".format(view))
-            fit_wls = dti_wls.fit(imagedict[view]["image"])
-            print("Extracting {0} FA.".format(view))
-            fa1 = fit_wls.fa
-            print("Extracting {0} EVALS.".format(view))
-            evals1 = fit_wls.evals
-            print("Extracting {0} EVECS.".format(view))
-            evecs1 = fit_wls.evecs
-            print("Extracting {0} Color FA.".format(view))
-            cfa1 = dti.color_fa(fa1, evecs1)
-            cfa1 = cfa1/cfa1.max()
-            print("Defining {0} renderer.".format(view))
-            render = fvtk.ren()
-            print("Generating {0} image.".format(view))
-            x =cfa1.shape[imagedict[view]["dropdim"][0]]
-            y =cfa1.shape[imagedict[view]["dropdim"][1]]
+    #Not necessary
+    # print("Resampling the brain to a standard resolution.")
+    # image, affine1 = reslice(image_masked, image.get_affine(), image.get_header().get_zooms()[:3], (3.0,3.0,3.0))
 
-            #print(x, y, 1, 3)
-            cfa2 = cfa1.reshape(x, y, 1, 3)
-            evals2 = evals1.reshape(x, y, 1, 3)
-            evecs2 = evecs1.reshape(x, y, 1, 3, 3)
+    print("Checking the image dimensions")
+    Xsize, Ysize, Zsize, directions = image.shape
+    print("X: {0}\nY: {1}\nZ: {2}".format(Xsize, Ysize, Zsize))
+    #If a slice is specified, it might be a good idea to double-check that the slice is a valid index.
+    #If so, we will extract that slice from the image.
 
-            fvtk.add(render, fvtk.tensor(evals2, evecs2, cfa2, sphere))
-            fvtk.record(render, out_path=arguments["--outprefix"]+"_"+view+".png", size=(800,800), magnification=2)
-            print("Image Saved")
-        #ren.clear()
+    #Define Image Scopes. This will need to be adjusted to grab the slice at the given index, not the middle one.
+    print("Defining the image scopes.")
+    imagedict = {"axial": {"dropdim": [0,1], "scope": (slice(0,Xsize), slice(0,Ysize), slice(math.floor(Zsize/2),math.floor(Zsize/2)+1))},
+                 "coronal": {"dropdim": [0,2], "scope": (slice(0,Xsize), slice(math.floor(Ysize/2),math.floor(Ysize/2)+1), slice(0, Zsize))},
+                 "sagittal": {"dropdim": [1,2], "scope": (slice(math.floor(Xsize/2),math.floor(Xsize/2)+1), slice(0,Ysize), slice(0, Zsize))}}
 
-    except:
-        print("Could not check orientation or output image.")
-        sys.exit(1)
+
+    #roi_idx = (slice(0,image.shape[0]), slice(0,image.shape[1]), slice(middleslice,middleslice+1))#(slice(0,image.shape[0]), slice(0,image.shape[1]), slice(int(image.shape[2]/2),int(image.shape[2]/2)+1))
+    # print("Defining sphere.")
+    # sphere = dpd.get_sphere('symmetric724')
+
+    #Slice the whole dataset by the scope
+    if arguments["--slice"] == None:
+        result = fit(image, gtab, fit_type=arguments["--fit"])
+    else:
+        result = fit(image[imagedict[arguments["--axis"]["scope"]]], gtab, fit_type=arguments["--fit"])
+
+    #Saving data will be done here. The rest of the stuff below is unnecessary.
+
+    #print("Slicing the dataset with the scopes.")
+    #data_to_fit = image[imagedict[view]["scope"]]
+    # for view in ["sagittal", "coronal", "axial"]:
+    #     imagedict[view]["image"] = image[imagedict[view]["scope"]]
+
+        # print("Fitting {0} data.".format(view))
+        # fit_wls = dti_wls.fit(imagedict[view]["image"])
+
+        # print("Extracting {0} FA.".format(view))
+        # fa1 = fit_wls.fa
+        # print("Extracting {0} EVALS.".format(view))
+        # evals1 = fit_wls.evals
+        # print("Extracting {0} EVECS.".format(view))
+        # evecs1 = fit_wls.evecs
+        # print("Extracting {0} Color FA.".format(view))
+        # cfa1 = dti.color_fa(fa1, evecs1)
+        # cfa1 = cfa1/cfa1.max()
+        # print("Defining {0} renderer.".format(view))
+        # render = fvtk.ren()
+        # print("Generating {0} image.".format(view))
+        # x =cfa1.shape[imagedict[view]["dropdim"][0]]
+        # y =cfa1.shape[imagedict[view]["dropdim"][1]]
+        #
+        # #print(x, y, 1, 3)
+        # cfa2 = cfa1.reshape(x, y, 1, 3)
+        # evals2 = evals1.reshape(x, y, 1, 3)
+        # evecs2 = evecs1.reshape(x, y, 1, 3, 3)
+        #
+        # fvtk.add(render, fvtk.tensor(evals2, evecs2, cfa2, sphere))
+        # fvtk.record(render, out_path=arguments["--outprefix"]+"_"+view+".png", size=(800,800), magnification=2)
+        # print("Image Saved")
     sys.exit(0)
 
 if __name__ == '__main__':
