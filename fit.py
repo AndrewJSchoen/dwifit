76f0e15412b1d5e119062d025fe3583d67f32c19
AndrewJSchoen <schoen.andrewj@gmail.com>
2016-06-08 10:49:09 -0500

    Fixes
    [1] Fixed docstring to be more helpful
    [2] image_data vs image fixes
    [3] Indenting

--- fit.py
+++ fit.py
@@ -9,7 +9,7 @@ from dipy.align.reslice import reslice
 from dipy.core.gradients import gradient_table
 from dipy.io import read_bvals_bvecs
 from docopt import docopt
-import dipy.data as dpd
+#import dipy.data as dpd
 reload(dti)
 
 import matplotlib.pyplot as plt
@@ -20,18 +20,20 @@ doc = """
 Orientation Check, Version {0}.
 
 Usage:
-    orientationcheck.py [options] --image=<FILE> --bval=<FILE> --bvec=<FILE> --outprefix=<FILE> [(--axis=sagittal | --axis=coronal | --axis=axial) --slice=<slice>] --fit_type=<type>
+    fit.py [options] --image=<FILE> --bval=<FILE> --bvec=<FILE> --outprefix=<FILE>
+    fit.py [options] --image=<FILE> --bval=<FILE> --bvec=<FILE> --outprefix=<FILE> --axis=<type> --slice=<slice>
 
 Options:
-    -h --help          Show this screen.
-    -v --version       Show version.
-    --image=<FILE>     Image File (path).
-    --bval=<FILE>      BVAL File (path).
-    --bvec=<FILE>      BVEC File (path).
-    --out=<FILE>       Output File (path).
-    --slice=<slice>    Specify a slice [default: None]
-    --axis=<type>      Specify an axis [default: coronal]
-    --fit_type=<type>  Specify a type (LS, NLLS, RT)[default: WLS]
+    -h --help            Show this screen.
+    -v --version         Show version.
+    --image=<FILE>       Image File (path).
+    --image_mask=<FILE>  Image Mask File (path). [default: None]
+    --bval=<FILE>        BVAL File (path).
+    --bvec=<FILE>        BVEC File (path).
+    --outprefix=<FILE>   Output File (path).
+    --slice=<slice>      Specify a slice [default: None]
+    --axis=<type>        Specify an axis (coronal, sagittal, axial) [default: coronal]
+    --fit_type=<type>    Specify a type (WLS, LS, NLLS, RT) [default: WLS]
 """.format(Version)
 
 #============================================================================
@@ -57,55 +59,69 @@ def exists(path):
 
 def fit(image, gtab, fit_type="WLS"):
     print("Generating the tensor model.")
-    dti_wls = dti.TensorModel(gtab, fit_method=fit_type)
+    dti_model = dti.TensorModel(gtab, fit_method=fit_type)
 
     #the fit method will take in the data, regardless of the shape
-    print("Fitting {0} data.".format(view))
-    fit_wls = dti_wls.fit(image)
+    print("Fitting data.")
+    fitted = dti_model.fit(image)
+    return fitted
 
 def run(rawargs):
     #retrieves arguments
     arguments = docopt(doc, argv=rawargs, version='Orientation Check v{0}'.format(Version))
+    print(arguments)
     inputs = [{"Value":"image file", "Flag": "--image"}, {"Value":"bvec file", "Flag": "--bvec"}, {"Value":"bvec file", "Flag": "--bvec"}]
+    if arguments["--image_mask"] != None and arguments["--image_mask"] != 'None':
+        inputs.append({"Value":"image mask file", "Flag": "--image_mask"})
     for inputinfo in inputs:
         if not exists(arguments[inputinfo["Flag"]]):
             print("The {0} specified does not exist!".format(inputinfo["Value"]))
             sys.exit(1)
 
-        #Load image
-        image = nib.load(arguments["--image"])
+    #Load image
+    image = nib.load(arguments["--image"])
+    image_data = image.get_data()
+    #Generates gradient table
+    print("Generating gradient table.")
+    bvals, bvecs = read_bvals_bvecs(arguments['--bval'], arguments['--bvec'])
+    gtab = gradient_table(*read_bvals_bvecs(arguments['--bval'], arguments['--bvec']))
 
-        #Generates gradient table
-        print("Generating gradient table.")
-        gtab = gradient_table(read_bvals_bvecs(arguments['--bval'], arguments['--bvec']))
-
-        print("Masking the brain.")
+    print("Masking the brain.")
+    if arguments["--image_mask"] != None and arguments["--image_mask"] != 'None':
+        #Mask the image with the mask provided
+        image_mask = nib.load(arguments["--image_mask"])
+        image_mask_data = image_mask.get_data()
+        image_masked = image_mask * image_data
+    else:
         image_masked, mask = median_otsu(image_data, 3, 1, autocrop=True, dilate=2)
 
-        print("Checking the image dimensions")
-        Xsize, Ysize, Zsize, directions = image.shape
-        print("X: {0}\nY: {1}\nZ: {2}".format(Xsize, Ysize, Zsize))
-        #If a slice is specified, double-check that the slice is a valid index
-        #If it is valid, extract that slice from the image
+    print("Checking the image dimensions")
+    Xsize, Ysize, Zsize, directions = image.shape
+    print("X: {0}\nY: {1}\nZ: {2}".format(Xsize, Ysize, Zsize))
+    #If a slice is specified, double-check that the slice is a valid index
+    #If it is valid, extract that slice from the image
 
-        if arguments["--slice"] == None or arguments["--slice"] == 'None':
-            result = fit(image, gtab, fit_type=arguments["--fit_type"])
+    if arguments["--slice"] == None or arguments["--slice"] == 'None':
+        result = fit(image_masked, gtab, fit_type=arguments["--fit_type"])
+    else:
+        arguments["--slice"] = int(arguments["--slice"])
+        if arguments["--axis"] == "sagittal":
+            axisbound = Xsize
+        elif arguments["--axis"] == "coronal":
+            axisbound = Ysize
         else:
-            arguments["--slice"] = int(arguments["--slice"])
-            if arguments["--axis"] == "sagittal":
-                axisbound = Xsize
-            elif arguments["--axis"] == "coronal":
-                axisbound = Ysize
-            else:
-                axisbound = Zsize
-            if 0 <= arguments["--slice"] <= axisbound:
-                print("Defining the image scopes.")
-                imagedict = {"axial": {"dropdim": [0,1], "scope": (slice(0,Xsize), slice(0,Ysize), slice(arguments["--slice"],arguments["--slice"]+1))},
-                             "coronal": {"dropdim": [0,2], "scope": (slice(0,Xsize), slice(arguments["--slice"],arguments["--slice"]+1), slice(0, Zsize))},
-                             "sagittal": {"dropdim": [1,2], "scope": (slice(arguments["--slice"],arguments["--slice"]+1), slice(0,Ysize), slice(0, Zsize))}}
-                result = fit(image[imagedict[arguments["--axis"]["scope"]]], gtab, fit_type=argument["--fit_type"])
-            else:
-                print("Slice does not exist on given axis.")
+            axisbound = Zsize
+        if 0 <= arguments["--slice"] <= axisbound:
+            print("Defining the image scopes.")
+            imagedict = {"axial": {"scope": (slice(0,Xsize), slice(0,Ysize), slice(arguments["--slice"],arguments["--slice"]+1))},
+                         "coronal": {"scope": (slice(0,Xsize), slice(arguments["--slice"],arguments["--slice"]+1), slice(0, Zsize))},
+                         "sagittal": {"scope": (slice(arguments["--slice"],arguments["--slice"]+1), slice(0,Ysize), slice(0, Zsize))}}
+            print(imagedict)
+            result = fit(image_masked[imagedict[arguments["--axis"]]["scope"]], gtab, fit_type=arguments["--fit_type"])
+        else:
+            print("Slice does not exist on given axis.")
+
+    print(result)
 
     sys.exit(0)
 
