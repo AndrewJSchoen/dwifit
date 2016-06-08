bd4e176400c15d5def9c7e4d5180d5fbf345f8fc
MPDean <michael.patrick.dean@gmail.com>
2016-06-08 09:42:14 -0500

    Changes Made to Structure
    Added axis specification
    Rearranged final if/else statement in order to check to see if index is
    real

--- fit.py
+++ fit.py
@@ -1,4 +1,4 @@
-#!/home/schoen/Scripts/perseus/ThirdParty/Python/bin/python
+#!../Python/bin/python
 import matplotlib.pyplot as plt
 import os, sys, pandas, math, numpy
 import nibabel as nib
@@ -9,7 +9,7 @@ from dipy.align.reslice import reslice
 from dipy.core.gradients import gradient_table
 from dipy.io import read_bvals_bvecs
 from docopt import docopt
-#import dipy.data as dpd
+import dipy.data as dpd
 reload(dti)
 
 import matplotlib.pyplot as plt
@@ -17,10 +17,10 @@ import matplotlib.pyplot as plt
 Version = "0.1"
 
 doc = """
-Model Fitting, Version {0}.
+Orientation Check, Version {0}.
 
 Usage:
-    orientationcheck.py [options] --image=<FILE> --bval=<FILE> --bvec=<FILE> --outprefix=<FILE> [(--axis sagittal | --axis coronal | --axis axial) --slice <slice>] --fit_type
+    orientationcheck.py [options] --image=<FILE> --bval=<FILE> --bvec=<FILE> --outprefix=<FILE> [(--axis=sagittal | --axis=coronal | --axis=axial) --slice=<slice>] --fit_type=<type>
 
 Options:
     -h --help          Show this screen.
@@ -29,8 +29,9 @@ Options:
     --bval=<FILE>      BVAL File (path).
     --bvec=<FILE>      BVEC File (path).
     --out=<FILE>       Output File (path).
-    --slice <slice>    Specify a slice [default: None]
-    --fit_type <Type>  Specify a type (LS, NLLS, RT)[default: WLS]
+    --slice=<slice>    Specify a slice [default: None]
+    --axis=<type>      Specify an axis [default: coronal]
+    --fit_type=<type>  Specify a type (LS, NLLS, RT)[default: WLS]
 """.format(Version)
 
 #============================================================================
@@ -58,102 +59,54 @@ def fit(image, gtab, fit_type="WLS"):
     print("Generating the tensor model.")
     dti_wls = dti.TensorModel(gtab, fit_method=fit_type)
 
-    ## the fit method will take in the data, regardless of shape.
+    #the fit method will take in the data, regardless of the shape
     print("Fitting {0} data.".format(view))
     fit_wls = dti_wls.fit(image)
 
-    return (fit_wls)
-
-
 def run(rawargs):
-    #This gets us our arguments. May need to be tweaked for the new ones we are adding.
+    #retrieves arguments
     arguments = docopt(doc, argv=rawargs, version='Orientation Check v{0}'.format(Version))
-    inputs = [{"Value":"image file", "Flag": "--image"}, {"Value":"bvec file", "Flag": "--bvec"}, {"Value":"bvec file", "Flag": "--bvec"}, {"Value":"slice", "Flag": "--slice"}, {"Value":"fit type", "Flag": "--fit_type"}]
+    inputs = [{"Value":"image file", "Flag": "--image"}, {"Value":"bvec file", "Flag": "--bvec"}, {"Value":"bvec file", "Flag": "--bvec"}]
     for inputinfo in inputs:
         if not exists(arguments[inputinfo["Flag"]]):
             print("The {0} specified does not exist!".format(inputinfo["Value"]))
             sys.exit(1)
 
+        #Load image
+        image = nib.load(arguments["--image"])
 
-    #Load image. Always do this.
-    image = nib.load(arguments["--image"])
+        #Generates gradient table
+        print("Generating gradient table.")
+        gtab = gradient_table(read_bvals_bvecs(arguments['--bval'], arguments['--bvec']))
 
-    #Create gradient table. Always do this.
-    print("Generating gradient table.")
-    gtab = gradient_table(read_bvals_bvecs(arguments['--bval'], arguments['--bvec']))
+        print("Masking the brain.")
+        image_masked, mask = median_otsu(image_data, 3, 1, autocrop=True, dilate=2)
 
-    #### MOVED ABOVE >>>>>
-    #Define the tensor model
-    # print("Generating the tensor model.")
-    # dti_wls = dti.TensorModel(gtab, fit_method="NLLS")
-    #### MOVED ABOVE <<<<<
+        print("Checking the image dimensions")
+        Xsize, Ysize, Zsize, directions = image.shape
+        print("X: {0}\nY: {1}\nZ: {2}".format(Xsize, Ysize, Zsize))
+        #If a slice is specified, double-check that the slice is a valid index
+        #If it is valid, extract that slice from the image
 
+        if arguments["--slice"] == None or arguments["--slice"] == 'None':
+            result = fit(image, gtab, fit_type=arguments["--fit_type"])
+        else:
+            arguments["--slice"] = int(arguments["--slice"])
+            if arguments["--axis"] == "sagittal":
+                axisbound = Xsize
+            elif arguments["--axis"] == "coronal":
+                axisbound = Ysize
+            else:
+                axisbound = Zsize
+            if 0 <= arguments["--slice"] <= axisbound:
+                print("Defining the image scopes.")
+                imagedict = {"axial": {"dropdim": [0,1], "scope": (slice(0,Xsize), slice(0,Ysize), slice(arguments["--slice"],arguments["--slice"]+1))},
+                             "coronal": {"dropdim": [0,2], "scope": (slice(0,Xsize), slice(arguments["--slice"],arguments["--slice"]+1), slice(0, Zsize))},
+                             "sagittal": {"dropdim": [1,2], "scope": (slice(arguments["--slice"],arguments["--slice"]+1), slice(0,Ysize), slice(0, Zsize))}}
+                result = fit(image[imagedict[arguments["--axis"]["scope"]]], gtab, fit_type=argument["--fit_type"])
+            else:
+                print("Slice does not exist on given axis.")
 
-    #Keep this here
-    print("Masking the brain.")
-    image_masked, mask = median_otsu(image_data, 3, 1, autocrop=True, dilate=2)
-    #If the user provides a mask, we could try to mask with it instead
-
-    #Not necessary
-    # print("Resampling the brain to a standard resolution.")
-    # image, affine1 = reslice(image_masked, image.get_affine(), image.get_header().get_zooms()[:3], (3.0,3.0,3.0))
-
-    print("Checking the image dimensions")
-    Xsize, Ysize, Zsize, directions = image.shape
-    print("X: {0}\nY: {1}\nZ: {2}".format(Xsize, Ysize, Zsize))
-    #If a slice is specified, it might be a good idea to double-check that the slice is a valid index.
-    #If so, we will extract that slice from the image.
-
-    #Define Image Scopes. This will need to be adjusted to grab the slice at the given index, not the middle one.
-    print("Defining the image scopes.")
-    imagedict = {"axial": {"dropdim": [0,1], "scope": (slice(0,Xsize), slice(0,Ysize), slice(math.floor(Zsize/2),math.floor(Zsize/2)+1))},
-                 "coronal": {"dropdim": [0,2], "scope": (slice(0,Xsize), slice(math.floor(Ysize/2),math.floor(Ysize/2)+1), slice(0, Zsize))},
-                 "sagittal": {"dropdim": [1,2], "scope": (slice(math.floor(Xsize/2),math.floor(Xsize/2)+1), slice(0,Ysize), slice(0, Zsize))}}
-
-
-    #roi_idx = (slice(0,image.shape[0]), slice(0,image.shape[1]), slice(middleslice,middleslice+1))#(slice(0,image.shape[0]), slice(0,image.shape[1]), slice(int(image.shape[2]/2),int(image.shape[2]/2)+1))
-    # print("Defining sphere.")
-    # sphere = dpd.get_sphere('symmetric724')
-
-    #Slice the whole dataset by the scope
-    if arguments["--slice"] == None:
-        result = fit(image, gtab, fit_type=arguments["--fit_type"])
-    else:
-        result = fit(image[imagedict[arguments["--axis"]["scope"]]], gtab, fit_type=arguments["--fit_type"])
-
-    #Saving data will be done here. The rest of the stuff below is unnecessary.
-
-    #print("Slicing the dataset with the scopes.")
-    #data_to_fit = image[imagedict[view]["scope"]]
-    # for view in ["sagittal", "coronal", "axial"]:
-    #     imagedict[view]["image"] = image[imagedict[view]["scope"]]
-
-        # print("Fitting {0} data.".format(view))
-        # fit_wls = dti_wls.fit(imagedict[view]["image"])
-
-        # print("Extracting {0} FA.".format(view))
-        # fa1 = fit_wls.fa
-        # print("Extracting {0} EVALS.".format(view))
-        # evals1 = fit_wls.evals
-        # print("Extracting {0} EVECS.".format(view))
-        # evecs1 = fit_wls.evecs
-        # print("Extracting {0} Color FA.".format(view))
-        # cfa1 = dti.color_fa(fa1, evecs1)
-        # cfa1 = cfa1/cfa1.max()
-        # print("Defining {0} renderer.".format(view))
-        # render = fvtk.ren()
-        # print("Generating {0} image.".format(view))
-        # x =cfa1.shape[imagedict[view]["dropdim"][0]]
-        # y =cfa1.shape[imagedict[view]["dropdim"][1]]
-        #
-        # #print(x, y, 1, 3)
-        # cfa2 = cfa1.reshape(x, y, 1, 3)
-        # evals2 = evals1.reshape(x, y, 1, 3)
-        # evecs2 = evecs1.reshape(x, y, 1, 3, 3)
-        #
-        # fvtk.add(render, fvtk.tensor(evals2, evecs2, cfa2, sphere))
-        # fvtk.record(render, out_path=arguments["--outprefix"]+"_"+view+".png", size=(800,800), magnification=2)
-        # print("Image Saved")
     sys.exit(0)
 
 if __name__ == '__main__':
