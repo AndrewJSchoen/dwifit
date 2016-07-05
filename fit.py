e6aaf97c01b7b4e20ed1e8559389ef1315d300dd
MPDean <michael.patrick.dean@gmail.com>
2016-07-05 09:27:53 -0500

    Class Structure
    1.] Made whole image/slice into their own subclasses of major fit class
    2.]Changed docopt statement by removing unnecessary items
    3.]Added new directories function for a place for the information
    
    Note:
    1.] I’m not really sure how to actually run and pass information
    through the docopt statement so you’ll have to help with that if and
    when you have time to do so.
    
    2.]I’m also not completely sure if the subclassing of this information
    is the proper way so you may want to check over that
    
    3.] If there’s anything that needs to be changed, taken away or added
    feel free to do whatever or let me know and I can see what I can do

--- fit.py
+++ fit.py
@@ -1,3 +1,60 @@
+#def run(rawargs):
+#    #retrieves arguments
+#    arguments = docopt(doc, argv=rawargs, version='Orientation Check v{0}'.format(Version))
+#    #print(arguments)
+#    inputs = [{"Value":"image file", "Flag": "--image"}, {"Value":"bvec file", "Flag": "--bvec"}, {"Value":"bvec file", "Flag": "--bvec"}]
+#    if arguments["--image_mask"] != None and arguments["--image_mask"] != 'None':
+#        inputs.append({"Value":"image mask file", "Flag": "--image_mask"})
+#    for inputinfo in inputs:
+#        if not exists(arguments[inputinfo["Flag"]]):
+#            print("The {0} specified does not exist!".format(inputinfo["Value"]))
+#            sys.exit(1)
+#
+#
+#        #Generate average signal, to be used in reverse calculation
+#        image_average_signal = np.mean(image_masked, axis=3)
+#
+#        #Define the paths to the output files
+#        spd_file_path = arguments["--outprefix"]+'_spd.nii.gz'
+#        estimate_file_path = arguments["--outprefix"]+'_ecc_estimated.nii.gz'
+#        error_file_path = arguments["--outprefix"]+'_ecc_error.nii.gz'
+#
+#        #Generate the SPD data (lower triangular of the symmetric matrix)
+#        spd_data = result.lower_triangular()
+#        #Predict the original data based on the resulting tensor data
+#        estimate_data = result.predict(gtab, S0=image_b0_average_masked)
+#        #Generate the difference between original and the predicted original
+#        error_data = np.absolute(image_masked - estimate_data)
+#
+#    #     spd_file_path = arguments["--outprefix"]+arguments["--axis"]+pad_val(arguments["--slice"])+'_spd.nii.gz'
+#    #     spd_data = result.lower_triangular()
+#    #     estimate_file_path = arguments["--outprefix"]+arguments["--axis"]+pad_val(arguments["--slice"])+'_ecc_estimated.nii.gz'
+#    #     #THE FOLLOWING DOES NOT WORK: image_mask is 3D, and result.predict(gtab) produces a 4D dataset. Will need to mask differently
+#    #     estimate_data = result.predict(gtab, S0=image_average_signal_slice)# * image_mask
+#    #     error_file_path = arguments["--outprefix"]+arguments["--axis"]+pad_val(arguments["--slice"])+'_ecc_error.nii.gz'
+#    #     error_data = numpy.absolute(image_masked[imagedict[arguments["--axis"]]["scope"]] - estimate_data)
+#
+#    print("Saving SPD image to "+spd_file_path)
+#    nib.save(nib.Nifti1Image(spd_data, image.get_affine()), spd_file_path)
+#    print("Saving estimated image to "+estimate_file_path)
+#    nib.save(nib.Nifti1Image(estimate_data, image.get_affine()), estimate_file_path)
+#    print("Saving error image to "+error_file_path)
+#    nib.save(nib.Nifti1Image(error_data, image.get_affine()), error_file_path)
+#
+#    #nib.save(nib.Nifti1Image(result.lower_triangular(), image.get_affine()), arguments["--outprefix"]+'_spd.nii.gz')
+#
+#
+#
+#    sys.exit(0)
+#
+#if __name__ == '__main__':
+#    args = sys.argv
+#    del args[0]
+#    run(args)
+#
+#
+#    ############################################################################
+
 #!../Python/bin/python
 import matplotlib.pyplot as plt
 import os, sys, pandas, math
@@ -20,232 +77,59 @@ doc = """
 Orientation Check, Version {0}.
 
 Usage:
-    fit.py [options] --image=<FILE> --bval=<FILE> --bvec=<FILE> --outprefix=<FILE> --model=<type> --fit_type=<type>
-    fit.py [options] --image=<FILE> --bval=<FILE> --bvec=<FILE> --outprefix=<FILE> --model=<type> --fit_type=<type> --axis=<type> --slice=<slice>
+    fit.py [options] --bval=<FILE> --bvec=<FILE> --outprefix=<FILE> --fit_type=<type> --data_type=<type> --image=<FILE>
 
 Options:
     -h --help            Show this screen.
     -v --version         Show version.
-    --image=<FILE>       Image File (path).
-    --image_mask=<FILE>  Image Mask File (path). [default: None]
     --bval=<FILE>        BVAL File (path).
     --bvec=<FILE>        BVEC File (path).
     --outprefix=<FILE>   Output File (path).
-    --model=<type>       Specify a model (DTI, DKI) [default: DTI]
-    --fit_type=<type>    Specify a type (WLS, LS, NLLS, RT) [default: WLS]
-    --slice=<slice>      Specify a slice [default: None]
-    --axis=<type>        Specify an axis (coronal, sagittal, axial) [default: coronal]
+    --fit_type=<type>    Specify a type (WLS, LS, NLLS, RT) [default: WLS].
+    --data_type=<type>   Specify type of data (Whole_Image, Slice) [default: Whole_Image].
+    --image=<FILE>       Image File (path).
 """.format(Version)
 
 #============================================================================
 #             General Utility
 #============================================================================
 
-def cleanPathString(path):
-  if path.endswith("/"):
-    path = path[:-1]
-  if path.startswith("="):
-    path = path[1:]
-  realpath = os.path.realpath(path)
-  return realpath
+class Fit(object):
 
-
-def exists(path):
-    #Shorthand for checking the existence of a file
-    if os.path.exists(cleanPathString(path)):
-        return(1)
-    else:
-        print("Error: Input file '{0}' not found!".format(path))
-        return(0)
-
-def dti_fit(image, mask, gtab, fit_type=arguments["--fit_type"]):
-    print("Generating the tensor model.")
-    dti_model = dti.TensorModel(gtab, fit_method=fit_type)
-
-    #the fit method will take in the data, regardless of the shape
-    print("Fitting data.")
-    fitted = dti_model.fit(image, mask=mask)
-    # print("Generating prediction.")
-    # prediction = dti_model.predict()
-    return fitted
-
-def dki_fit(image, mask, gtab):
-    sigma = estimate_sigma(image, N=4))
-    den = nlmeans(image, sigma=sigma, mask=mask)
-
-    print("Generating Kurtosis Model.")
-    dkimodel = dki.DiffusionKurtosisModel(gtab)
-    dkifit = dkimodel.fit(den)
-
-
-def pad_val(val, pad_length=3):
-    if type(val) == str:
-        return val.zfill(pad_length)
-    elif type(val) == int:
-        replace_string = "{0:0"+str(pad_length)+"d}"
-        return replace_string.format(val)
-
-def run(rawargs):
-    #retrieves arguments
-    arguments = docopt(doc, argv=rawargs, version='Orientation Check v{0}'.format(Version))
-    #print(arguments)
-    inputs = [{"Value":"image file", "Flag": "--image"}, {"Value":"bvec file", "Flag": "--bvec"}, {"Value":"bvec file", "Flag": "--bvec"}]
-    if arguments["--image_mask"] != None and arguments["--image_mask"] != 'None':
-        inputs.append({"Value":"image mask file", "Flag": "--image_mask"})
-    for inputinfo in inputs:
-        if not exists(arguments[inputinfo["Flag"]]):
-            print("The {0} specified does not exist!".format(inputinfo["Value"]))
-            sys.exit(1)
-
-    #Load image
-    image = nib.load(arguments["--image"])
-    image_data = image.get_data()
-
-    #Generate gradient table
-    print("Generating gradient table.")
-    bvals, bvecs = read_bvals_bvecs(arguments['--bval'], arguments['--bvec'])
-    gtab = gradient_table(bvals, bvecs)
-
-
-    #Generate an image that is an average of all b0 volume (useful for reverse prediction)
-    print("Generating signal intensity map.")
-    values = np.array(bvals)
-    ii = np.where(values == bvals.min())[0]
-    image_b0_average = np.mean(image_data[:,:,:,ii], axis=3)
-
-    print("Masking the brain.")
-    if arguments["--image_mask"] != None and arguments["--image_mask"] != 'None':
-        #Mask the image with the mask provided
-        image_mask = nib.load(arguments["--image_mask"])
-        image_mask_data = image_mask.get_data()
-        #THE FOLLOWING DOES NOT WORK: image_data is 4D, and image_mask is likely 3D. Will need to mask differently
-        image_masked = image_mask_data * image_data
-    else:
-        image_masked, image_mask = median_otsu(image_data, 3, 1, autocrop=False, dilate=2)
-        image_b0_average_masked = image_mask * image_b0_average
-
-    print("Checking the image dimensions")
-    Xsize, Ysize, Zsize, directions = image.shape
-    print("X: {0}\nY: {1}\nZ: {2}".format(Xsize, Ysize, Zsize))
-    #If a slice is specified, double-check that the slice is a valid index
-    #If it is valid, extract that slice from the image
-
-
-    if arguments["--slice"] == None or arguments["--slice"] == 'None':
-        #Fit the data
-        result = fit(image_data, image_mask, gtab, fit_type=arguments["--fit_type"])
-
-        #Generate average signal, to be used in reverse calculation
-        image_average_signal = np.mean(image_masked, axis=3)
-
-        #Define the paths to the output files
-        spd_file_path = arguments["--outprefix"]+'_spd.nii.gz'
-        estimate_file_path = arguments["--outprefix"]+'_ecc_estimated.nii.gz'
-        error_file_path = arguments["--outprefix"]+'_ecc_error.nii.gz'
-
-        #Generate the SPD data (lower triangular of the symmetric matrix)
-        spd_data = result.lower_triangular()
-        #Predict the original data based on the resulting tensor data
-        estimate_data = result.predict(gtab, S0=image_b0_average_masked)
-        #Generate the difference between original and the predicted original
-        error_data = np.absolute(image_masked - estimate_data)
-
-
-    else:
-        #Force the argument to be in integer form
-        arguments["--slice"] = int(arguments["--slice"])
-
-        #Do some checking on the boundaries. Exit if not in bounds.
-        if arguments["--axis"] == "sagittal":
-            axisbound = Xsize
-        elif arguments["--axis"] == "coronal":
-            axisbound = Ysize
-        else:
-            axisbound = Zsize
-        if 0 > arguments["--slice"] or arguments["--slice"] > axisbound:
-            raise IOError("Slice does not exist on given axis.")
-            sys.exit(1)
-
-        #Slice all the datasets required
-        if arguments["--axis"] == "axial":
-            image_data_slice = image_data[:,:,arguments["--slice"]]
-            image_mask_slice = image_mask[:,:,arguments["--slice"]]
-        elif arguments["--axis"] == "sagittal":
-            image_data_slice = image_data[:,arguments["--slice"],:]
-            image_mask_slice = image_mask[:,arguments["--slice"],:]
-        else:
-            image_data_slice = image_data[arguments["--slice"],:,:]
-            image_mask_slice = image_mask[arguments["--slice"],:,:]
-
-        #Fit the data
-        result = fit(image_data_slice, image_mask_slice, gtab, fit_type=arguments["--fit_type"])
-
-        #Generate average signal, to be used in reverse calculation
-        image_average_signal_slice = np.mean(image_data_slice*image_mask_slice, axis=3)
-
-    #print(result.lower_triangular())
-    # if arguments["--slice"] == None or arguments["--slice"] == 'None':
-    #
-    # else:
-    #     spd_file_path = arguments["--outprefix"]+arguments["--axis"]+pad_val(arguments["--slice"])+'_spd.nii.gz'
-    #     spd_data = result.lower_triangular()
-    #     estimate_file_path = arguments["--outprefix"]+arguments["--axis"]+pad_val(arguments["--slice"])+'_ecc_estimated.nii.gz'
-    #     #THE FOLLOWING DOES NOT WORK: image_mask is 3D, and result.predict(gtab) produces a 4D dataset. Will need to mask differently
-    #     estimate_data = result.predict(gtab, S0=image_average_signal_slice)# * image_mask
-    #     error_file_path = arguments["--outprefix"]+arguments["--axis"]+pad_val(arguments["--slice"])+'_ecc_error.nii.gz'
-    #     error_data = numpy.absolute(image_masked[imagedict[arguments["--axis"]]["scope"]] - estimate_data)
-
-    print("Saving SPD image to "+spd_file_path)
-    nib.save(nib.Nifti1Image(spd_data, image.get_affine()), spd_file_path)
-    print("Saving estimated image to "+estimate_file_path)
-    nib.save(nib.Nifti1Image(estimate_data, image.get_affine()), estimate_file_path)
-    print("Saving error image to "+error_file_path)
-    nib.save(nib.Nifti1Image(error_data, image.get_affine()), error_file_path)
-
-    #nib.save(nib.Nifti1Image(result.lower_triangular(), image.get_affine()), arguments["--outprefix"]+'_spd.nii.gz')
-
-
-
-    sys.exit(0)
-
-if __name__ == '__main__':
-    args = sys.argv
-    del args[0]
-    run(args)
-
-
-    ############################################################################
-
-
-class wholeImageFit:
-
-    def __init__(self, rawDWI, bval, bvec, outprefix, image_mask, fit_method):
-        self.dwi_path = rawDWI
-        self.bval_path = bval
-        self.bvec_path = bvec
+    def __init__(self, bvals, bvecs, outprefix, fit_method='WLS', data_type='Whole_Image', dwi_data):
+        self.bval = bvals
+        self.bvec = bvecs
         self.output_root_path = outprefix
-        self.image_mask_path = image_mask_path
+        self.fit_method = fit_method
+        self.raw_data = dwi_data
 
+        #set up directories for storage of information and data
+        self.output_data = self.output_root_path + "/data" #directory where all files are stored
+        self.raw_data = self.output_data + "/dwi.nii.gz"
+        self.mask = self.output_data + "/mask.nii.gz"
+        self.masked_dwi = self.output_data + "/dwi.mask.nii.gz"
+        self.bval = self.output_data + "/bvals"
+        self.bvec = self.output_data + "/bvecs"
+        self.tenfit_img = self.output_data + "/dti.tensor.nii.gz"
+        self.dti_res_img = self.output_data + "/dti.res.nii.gz"
+        self.kurtosis_img = self.output_data + "/dki.model.nii.gz"
+        self.dki_res_img = self.output_data + "/dki.res.nii.gz"
 
-    def createMask(self):
-        if os.path.exists(self.dwi_path):
-            img = nib.load(self.dwi_path)
-            data = img.get_data()
-            masked_data, mask = median_otsu(data, 2,2)
+    def setupDirectories(self):
 
-            #Save those files
-            masked_img = nib.Nifti1Image(masked_data.astype(np.float32), img.get_affine())
-            mask_img = nib.Nifit1Image(mask.astype(np.float32), img.get_affine())
+        if not os.path.exists(self.output_root_path):
+            os.mkdir(self.output_root_path)
 
-            nib.save(masked_img, self.masked_dwi)
-            nib.save(mask_img, self.mask)
+        if not os.path.exists(self.output_preprocess):
+            os.mkdir(self.output_preprocess)
+
 
     def TensorFit(self):
         if os.path.exists(self.masked_dwi):
             img = nib.load(self.masked_dwi)
             data = img.get_data()
 
-            bvals, bvecs = read_bvals_bvecs(self.bval_path, self.bvec_path)
+            bvals, bvecs = read_bvals_bvecs(self.bval, self.bvec)
             gtab = gradient_table(bvals, bvecs)
 
             values = np.array(bvals)
@@ -271,7 +155,7 @@ class wholeImageFit:
             img = nib.load(self.masked_dwi)
             data = img.get_data()
 
-            bvals, bvecs = read_bvals_bvecs(self.bval_path, self.bvec_path)
+            bvals, bvecs = read_bvals_bvecs(self.bval, self.bvec)
             gtab = gradient_table(bvals, bvecs)
 
             values = np.array(bvals)
@@ -289,74 +173,41 @@ class wholeImageFit:
             nib.save(Kfit_img, self.kurtosis_img)
             nib.save(res_img, self.dki_res_img)
 
-class sliceImageFit:
+class WholeImage(Fit):
 
-    def __init__(self, slice, bval, bvec, outprefix, image_mask, fit_method):
-        self.slice_path = slice_image
-        self.bval_path = bval
-        self.bvec_path = bvec
-        self.output_root_path = outprefix
-        self.image_mask_path = image_mask
+    def __init__(self, rawDWI):
+        Fit.__init__(self, bvals, bvecs, outprefix, fit_method='WLS', "Whole_Image")
+        self.raw_data = rawDwi
 
     def createMask(self):
-        if os.path.exists(self.slice_path):
-            img = nib.load(self.slice_path)
+        if os.path.exists(self.raw_data):
+            img = nib.load(self.raw_data)
             data = img.get_data()
-            masked_data, data = median_otsu(data, 2,2)
+            masked_data, mask = median_otsu(data, 2,2)
 
-            masked_img = nib.Nifti1Image(masked_img.astype(np.float32), img.get_affine())
-            mask_img = nib.Nifti1Image(mask_img.astype(np.float32), img.get_affine())
+            #Save those files
+            masked_img = nib.Nifti1Image(masked_data.astype(np.float32), img.get_affine())
+            mask_img = nib.Nifit1Image(mask.astype(np.float32), img.get_affine())
 
             nib.save(masked_img, self.masked_dwi)
-            nib.save(mask_img, self.mask_dwi)
+            nib.save(mask_img, self.mask)
 
-    def TensorFit(self):
-        if os.path.exists(self.masked_dwi):
-            img = nib.load(self.masked_dwi)
+
+class SliceImage(Fit):
+
+    def __init__(self, slice_data):
+        Fit.__init__(self, bvals, bvecs, outprefix, fit_method='WLS', "Slice")
+        self.raw_data = slice_data
+
+    def createMask(self):
+        if os.path.exists(self.raw_data):
+            img = nib.load(self.raw_data)
             data = img.get_data()
+            masked_data, mask = median_otsu(data, 2,2)
 
-            bvals, bvecs = read_bvals_bvecs(self.bval_path, self.bvec_path)
-            gtab = gradient_table(bvals, bvecs)
+            #Save those files
+            masked_img = nib.Nifti1Image(masked_data.astype(np.float32), img.get_affine())
+            mask_img = nib.Nifit1Image(mask.astype(np.float32), img.get_affine())
 
-            values = np.array(bvals)
-            ii = np.where(values == bvals.min())[0]
-            b0_average = np.mean(data[:,:,:,ii], axis=3)
-
-            tenmodel = dti.TensorModel(gtab, fit_method="fit_method")
-            tenfit = tenmodel.fit(data)
-
-            estimate_data = tenmodel.predict(gtab, S0=b0_average)
-            residuals = np.absolute(data - estimate_data)
-
-            fit_img = nib.Nifti1Image(tenfit.astype(np.float32), img.get_affine())
-            res_img = nib.Nifti1Image(residuals.astype(np.float32), img.get_affine())
-            nib.save(fit_img, self.tenfit_img)
-            nib.save(res_img, self.dti_res_img)
-
-    def KurtosisModelFit(self):
-        if os.path.exists(self.masked_dwi):
-            img = nib.load(self.masked_dwi)
-            data = img.get_data()
-
-            bvals, bvecs = read_bvals_bvecs(self.bval_path, self.bvec_path)
-            gtab = gradient_table(bvals, bvecs)
-
-            values = np.array(bvals)
-            ii = np.where(walues == bvals.min())[0]
-            b0_average = np.mean(data[:,:,:,ii], axis=3)
-
-            dkimodel = dki.DiffusionKurtosisModel(gtab)
-            dkifit =  dkimodel.fit(data)
-
-            estimate_data = dkimodel.predict(gtab, S0=b0_average)
-            residuals = np.absolute(data - estimate_data)
-
-            Kfit_img = nib.Nifti1Image(dkifit.astype(np.float32), img.get_affine())
-            res_img = nib.Nifti1Image(residuals,astype(np.float32), img.get_affine())
-            nib.save(Kfit_img, self.kurtosis_img)
-            nib.save(res_img, self.dki_res_img)
-
-
-#Note:
-#What we will need to do next is set up a seperate file to run these functions from and if we want to incorporate docopt for the functions
-#Or we can run these functions within the same file
+            nib.save(masked_img, self.masked_dwi)
+            nib.save(mask_img, self.mask)
